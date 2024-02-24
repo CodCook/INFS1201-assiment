eb77f2cc4f5ce3ef033ad3e1ac48a831ddb9b7a3
CodCook <60304062@udst.edu.qa>
2024-02-24 16:23:36 +0300

    finsh ex5

--- q5.py
+++ q5.py
@@ -1,4 +1,37 @@
-
 def topGoalScorers(goalData):
-    for l in range(len(goalData)):
-        pass
+    '''this function return the top goal scorers'''
+    length = len(goalData)
+    team = 0
+    player = 1
+    goals = 2
+    lst = []
+
+    while goals < length:
+
+        if goalData[goals] > goalData[goals + 3]:
+            lst.append((goalData[team], goalData[player], goalData[goals]))
+        else:
+            lst.append((goalData[team], goalData[player + 3], goalData[goals + 3]))
+
+        team += 6
+        player += 6
+        goals += 6
+
+    top_team_score = lst[0][2]
+    topScorers = []
+
+    for j in range(len(lst)):
+        if lst[j][2] >= top_team_score:
+            topScorers.append(lst[j])
+            top_team_score = lst[j][2]
+
+    return topScorers
+
+
+
+#Test cases
+# Sample Run
+goalData = ['Qatar', 'Hassan Al-Haydos', 3, 'Qatar', 'Akram Afif', 1, 'Iraq', 'Amir Al-Ammari', 2, 'Iraq', 'Hussein Ali', 1, 'Jordan', 'Mahmoud Al-Mardi', 2, 'Jordan', 'Musa Al-Taamari', 1, 'Palestine', 'Oday Dabbagh', 3, 'Palestine', 'Bader Nasser', 1]
+
+print(topGoalScorers(goalData))
+# Expected output: [('Qatar', 'Hassan Al-Haydos', 3), ('Palestine', 'Oday Dabbagh', 3)]
--- try.py
+++ try.py
@@ -1,2 +1,7 @@
 
 
+tup = [('mohamed','name',6),(1,2,3)]
+rand = 9
+for i in range(len(tup)):
+    if tup[i][2]>3:
+        print('true')
